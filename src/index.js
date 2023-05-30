f38bf48f040f56909a2e7c23d28a74f171253d24
Olguta Fratila <fratilaolguta150@gmail.com>
2023-05-30 11:49:48 +0300

    add a dialog window when click add project btn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 
 console.log("let's get started");
+
+const modal = document.querySelector('.modal');
+const openModal = document.querySelector('.create-new-project-btn');
+const closeModal = document.querySelector('.close-modal');
+
+openModal.addEventListener('click', () => {
+    modal.showModal();
+});
+
+closeModal.addEventListener('click', () => {
+    modal.close();
+});
